0afeab3c93e854d1c54162efb1edacea12e35f85
lvaskys <lvaskys@gmail.com>
2022-01-23 04:29:35 -0600

    patches for d3d11 support for qsv

--- build/ffmpeg_extra.sh
+++ build/ffmpeg_extra.sh
@@ -3,4 +3,14 @@ _pre_configure(){
     # Apply a patch from ffmpeg's patchwork site.
     do_patch "https://raw.githubusercontent.com/TheElixZammuto/sunshine-prebuilt/master/build/0001-patch-idr-on-amf.patch" patch
     #
+	
+	patch -p1 -i "$LOCALBUILDDIR/0002-d3d11-qsv.patch"
+	patch -p1 -i "$LOCALBUILDDIR/0003-d3d11-qsv.patch"
+	patch -p1 -i "$LOCALBUILDDIR/0004-d3d11-qsv.patch"
+	patch -p1 -i "$LOCALBUILDDIR/0005-d3d11-qsv.patch"
+	patch -p1 -i "$LOCALBUILDDIR/0006-d3d11-qsv.patch"
+	patch -p1 -i "$LOCALBUILDDIR/0007-d3d11-qsv.patch"
+	patch -p1 -i "$LOCALBUILDDIR/0008-d3d11-qsv.patch"
+	patch -p1 -i "$LOCALBUILDDIR/0009-d3d11-qsv.patch"
+	patch -p1 -i "$LOCALBUILDDIR/0010-d3d11-qsv.patch"
 }
